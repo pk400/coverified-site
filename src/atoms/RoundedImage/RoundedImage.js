cec8d9416c5af6286a6bb74af35a5ad022ede55a
Joel Aro <joelaro.to@gmail.com>
2020-06-25 20:20:56 -0400

    Removed the word 'picture' in alt tag.

--- src/atoms/RoundedImage/RoundedImage.js
+++ src/atoms/RoundedImage/RoundedImage.js
@@ -5,7 +5,7 @@ import './RoundedImage.css'
 const RoundedImage = ({width, src}) => {
   return (
     <div className='image-container'>
-      <img src={src} width={width} alt='Team member picture' />
+      <img src={src} width={width} alt='Team member' />
     </div>
   )
 }
